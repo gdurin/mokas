b1228878069651cf46ed1ca9a0ec124148b1fa53
Domain Wall <gf.durin@gmail.com>
2016-06-10 13:09:14 +0200

    * Fix a bug on the filtering when used 'None' in parameters * Added run file for simulations: run_creep_simulations.py

---
+++ run_creep_simulations.py
@@ -0,0 +1,23 @@
+import os
+from run_creep import Creep 
+
+def isIniFile(filename):
+	return os.path.isfile(filename)
+
+if __name__ == "__main__":
+	myChoice = ['D0','D0.5'][1]
+	if myChoice == 'D0':
+		iniFilepath = "/home/wall/src/mumax3/bhaskar/bubble/finalSimulation/05062016/disorder/disorder_D0_sim.ini"
+		# Select the OoP field
+		Bz = -20
+	elif myChoice == 'D0.5':
+		iniFilepath = "/home/wall/src/mumax3/bhaskar/bubble/finalSimulation/05062016/disorder/disorder_D0.5_sim.ini"
+		# Select the OoP field
+		Bz = -10
+
+	if not isIniFile(iniFilepath):
+		print("There is a problem with the ini file {}: file not found".format(iniFilepath))
+		sys.exit()
+
+	creep_data = Creep(iniFilepath, Bz)
+	creep_data.plot_results()
